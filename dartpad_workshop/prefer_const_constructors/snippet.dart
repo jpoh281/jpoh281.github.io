7bbe169689f300f14cef106c1bb364c55ab46378
jpoh281 <54665433+jpoh281@users.noreply.github.com>
2022-03-01 15:59:57 +0900

    Update snippet.dart

--- dartpad_workshop/prefer_const_constructors/snippet.dart
+++ dartpad_workshop/prefer_const_constructors/snippet.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
